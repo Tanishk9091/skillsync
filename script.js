8990f0582eca405b117c47bf00caaa254313e8a0
Tanisha <sonitanisha1911@gmail.com>
2025-08-20 14:50:39 +0530

    changes in css

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-const sortBtns = document.querySelectorAll(".job-id > *")
+const sortBtns = document.querySelectorAll(".job-id > *");
+const sortItems = document.quesrySelectorAll(".jobs-container > *")
 
-sortBtns.forEach((btn) =>{
-    btn.addEventListener('click', () =>{
-       sortBtns.forEach((btn) => btn.classList.remove
-        ("active"));
+
+sortBtns.forEacg((btn) => {
+    btn.addEventListener("click",() => {
+        sortBtns.forEach((btn) => btn.classList.remove("active"));
         btn.classList.add("active");
+
+        const targetData = btn.getAttribute("data-target");
+
+        sortItems.forEach((item) => {
+            item.classList.add("delete");
+            if(item.getAttribute("data-item") === targetData || targetData === "all"){
+                item.classList.remove("delete");
+
+
+            }
+        });
     });
 });
